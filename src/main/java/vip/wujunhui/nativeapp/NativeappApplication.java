81fa303aa61792a3e263f0c6a82a4dc26604865a
wjh <1508710375@qq.com>
2025-04-02 11:05:14 +0800

    feat: init

---
+++ src/main/java/vip/wujunhui/nativeapp/NativeappApplication.java
@@ -0,0 +1,13 @@
+package vip.wujunhui.nativeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vip/wujunhui/nativeapp/NativeappApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.wujunhui.nativeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
